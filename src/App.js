2c575bcc5815edbf3c4e21c92bf8ec0dc8f2a467
Vinipyh31 <52539401+Vinipyh31@users.noreply.github.com>
2022-10-20 10:43:37 +0500

    make component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+const Data = [
+  { name: 'Sold', color: '#BD1FBE', value: 677 },
+  { name: 'Got free', color: '#FC64FF', value: 23 },
+  { name: 'Burned', color: 'lime', value: 202 },
+  { name: 'Free float', color: 'gray', value: 323 },
+  { name: 'Zero', color: 'black', value: 0 },
+]
+
+
+function Bar({ width, height, color }) {
+  return (
+    <div
+      className='bar'
+      style={{
+        width: width,
+        height: height,
+        backgroundColor: color,
+      }}
+    >
+    </div>
+  )
+}
+
+function ProgressBar({ items, width, height }) {
+
+  const filteredItems = items.filter((item) => item.value !== 0);
+  const sumOfValues = filteredItems.reduce((prev, obj) => obj.value + prev, 0);
+  const heightForRows = height * 0.4;
+
+  const maxBars = Math.floor(width / 10);
+
+  return (
+    <div className='progress-bar'
+      style={{
+        width: width,
+        height: height,
+      }}>
+      <div className='bars'>
+        {filteredItems.map((item) => {
+          const percent = item.value / (sumOfValues * 0.01)
+          let numOfBars = Math.round(maxBars * 0.01 * percent);
+          if (!numOfBars) numOfBars = 1;
+          return [...Array(numOfBars)].map(() => <Bar height={heightForRows} color={item.color} />)
+        })}
+      </div>
+      <div className='description'>
+        {filteredItems.map((item) =>
+          <div className='description__item'>
+            <div
+              className='circle'
+              style={{
+                width: heightForRows,
+                height: heightForRows,
+                backgroundColor: item.color
+              }}
+            ></div> {`${item.name}: ${item.value} (${(item.value / (sumOfValues * 0.01)).toFixed(1)}%)`}
+          </div>)}
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar items={Data} height={50} width={800} />
     </div>
   );
 }
